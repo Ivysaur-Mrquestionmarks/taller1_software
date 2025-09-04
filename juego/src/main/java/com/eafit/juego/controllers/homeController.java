ed0071701ead43aacf90694f67dcf44efe221a64
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2025-09-04 07:21:03 -0500

    Create homeController.java

---
+++ juego/src/main/java/com/eafit/juego/controllers/homeController.java
@@ -0,0 +1,21 @@
+package com.eafit.juego.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+public class homeController {
+
+   @GetMapping("/")
+   public String index(Model model) {
+       model.addAttribute("title", "Bienvenido al CABA");
+       model.addAttribute("subtitle", "Juegos del torneo de football de verano");
+       return "home/index";
+   }  
+    
+}
