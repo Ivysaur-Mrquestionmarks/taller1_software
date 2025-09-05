8bb681a67330789a346ba217f9e935e65545d32d
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2025-09-05 08:44:16 -0500

    vista 1

--- juego/src/main/java/com/eafit/juego/ServletInitializer.java
+++ juego/src/main/java/com/eafit/juego/ServletInitializer.java
@@ -1,7 +1,9 @@
 package com.eafit.juego;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
--- juego/src/main/java/com/eafit/juego/controllers/homeController.java
+++ juego/src/main/java/com/eafit/juego/controllers/homeController.java
@@ -1,14 +1,13 @@
 package com.eafit.juego.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+@Controller
 public class homeController {
 
    @GetMapping("/")
